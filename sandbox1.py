ce7eac3d2c6e0573d50069a44ef5af065dccfde3
SUHAAS-sys <85066672+SUHAAS-sys@users.noreply.github.com>
2021-06-03 08:43:34 +0530

    HAT

--- sandbox1.py
+++ sandbox1.py
@@ -1,4 +1,2 @@
-# Sandbox area to test out code snippets before inclusion in cryptogram_solver.py.
-# Leave in code under testing, comment out code if needed for later
-# reference, or delete code that is no
-print("a,b,c")
+A=('hat fat rat')
+print(A,type(A))
